acc0c4cd84c35d0ed764475cf49f2ec839dbde86
mucsinyusuf <muxsinyussuf@gmail.com>
2024-07-08 06:19:25 -0400

    airbnbclone

---
+++ airbnb/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+ function Navbar() {
+    return (
+        <nav>
+            <img src="/images/airbnblogo1.png" alt="airbnb" className=""nav--logo />
+        </nav>
+    )
+}
+export default Navbar;
